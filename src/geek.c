9f5786d4ef4bc2a0828e34a945d15052161baf3b
Hunter Blaze <25360019+blazegeek@users.noreply.github.com>
2020-12-29 15:23:55 -0500

    Update geek.c

--- src/geek.c
+++ src/geek.c
@@ -6,19 +6,13 @@
 
 #include "sha3/sph_blake.h"
 #include "sha3/sph_bmw.h"
-#include "sha3/sph_groestl.h"
-#include "sha3/sph_jh.h"
-#include "sha3/sph_keccak.h"
-#include "sha3/sph_skein.h"
-#include "sha3/sph_luffa.h"
-#include "sha3/sph_cubehash.h"
-#include "sha3/sph_shavite.h"
-#include "sha3/sph_simd.h"
 #include "sha3/sph_echo.h"
-#include "sha3/sph_hamsi.h"
-#include "sha3/sph_fugue.h"
 #include "sha3/sph_shabal.h"
-#include "sha3/sph_whirlpool.h"
+#include "sha3/sph_groestl.h"
+#include "sha3/sph_cubehash.h"
+#include "sha3/sph_keccak.h"
+#include "sha3/sph_hamsi.h"
+#include "sha3/sph_simd.h"
 
 void geek_hash(const char* input, char* output, uint32_t len)
 {
